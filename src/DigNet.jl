9720859eec9a86ee5229d8b8bff53f5bbf090b4a
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-02 21:31:41 +0800

    basic: Variable & Function

--- src/DigNet.jl
+++ src/DigNet.jl
@@ -1,5 +1,18 @@
 module DigNet
 
-greet() = print("Hello World!")
+struct Variable
+    data
+end
+
+function Function(f)
+    forward = function(input)
+        x = input.data
+        y = f(x)
+        return Variable(y)
+    end
+end
+
+mul2 = Function(funciton(x) x*2 end)
+mul2(Variable(3))
 
 end # module
