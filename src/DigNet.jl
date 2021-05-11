662d87b9698fd85eab864f70b8d635fcdc061f20
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-12 03:04:04 +0800

    ForwardDiff

--- src/DigNet.jl
+++ src/DigNet.jl
@@ -1,15 +1,3 @@
 module DigNet
 
-export Var, Func
-
-struct Var
-    data
-    grad
-    Var(data) = new(data, nothing)
 end
-
-function Func(f)
-    input -> Var(f(input.data))
-end
-
-end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,33 +1,17 @@
-#=
-runtests:
-- Julia version: 
-- Author: dannypsnl
-- Date: 2021-05-02
-=#
-
-using DigNet
+using ForwardDiff
 using Test: @testset, @test
 
-mul2 = Func(x -> x * 2)
-Exp = Func(x -> broadcast(exp, x))
-square = Func(x -> broadcast(x -> x ^ 2, x))
-
-@testset "Forward Function" begin
-    x = Var([1, 2, 3])
-    @test mul2(x).data == [2, 4, 6]
-    @test Exp(x).data == [2.718281828459045, 7.38905609893065, 20.085536923187668]
-    @test square(x).data == [1, 4, 9]
+function squareroot(x) #pretending we don't know sqrt()
+    z = copy(x) # Initial starting point for Newton’s method
+    while abs(z*z - x) > 1e-13
+        z = z - (z*z-x)/(2z)
+    end
+    return z
 end
 
 @testset "Diff" begin
-    numberical_diff(f, x, eps = 1e-4) = begin
-         x0 = Var(broadcast(x -> x - eps, x.data))
-         x1 = Var(broadcast(x -> x + eps, x.data))
-         y0 = f(x0)
-         y1 = f(x1)
-         return (y1.data - y0.data) / (2 * eps)
-    end
-    f(x) = square(Exp(square(x)))
-    x = Var([0.5])
-    @test numberical_diff(f, x) == [3.2974426293297388]
+    @test squareroot(2.0) == 1.4142135623730951
+    dsqrt(x) = ForwardDiff.derivative(squareroot, x)
+    dsqrt(2.0)
+    @test dsqrt(2.0) == 0.35355339059327373
 end
